754a25c80925fd6ab983a341feb8dbfe0ecb07a7
Hugo Alexander Hernandez Serrano <alexandre_2420@outlook.com>
2021-09-24 09:35:10 -0500

    config globalStyles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,35 @@
+
+import { createGlobalStyle } from "styled-components";
+
+//variables globales
+export enum GlobalVariables {
+    white = '#ffffff',
+    black = '#2F527B',
+    blue = '#6066d0d9',
+    success = '#60BF88',
+    error = '#EA8282',
+    orange = '#F9A826'
+  }
+
+const GlobalStyles  = createGlobalStyle `
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+   
+    html {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 62.5%;
+    }
+    h1 {
+        font-size: 3.6rem;
+    }
+    h2 {
+        font-size: 1.8rem;
+    }
+    h4 {
+        font-size: 1.4rem;
+    }
+`
+export default GlobalStyles
